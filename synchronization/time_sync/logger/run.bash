ba59e12926682ff861d338b0023cf72a939cc52c
algof <algofkz@gmail.com>
2025-12-20 01:48:56 +0700

    don

---
+++ synchronization/time_sync/logger/run.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 logger.py --bind 0.0.0.0 --port 5900 --expect 10
---
+++ synchronization/time_sync/node_a/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 peer_node.py \
+	  --name A --listen 0.0.0.0 5002 \
+	  --peers B@192.168.122.3:5003 C@192.168.122.4:5004 D@192.168.122.5:5005 \
+	  --logger 192.168.122.6 5900 \
+	  --offset-ms 600 \
+	  --initiate-broadcast --msg "Hello from A"
+
