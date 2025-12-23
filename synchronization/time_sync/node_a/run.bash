14216408533a73e24e7ebc403b2d1021cc0d52d9
algof <algofkz@gmail.com>
2025-12-23 11:13:34 +0700

    edit offset-ms at node a, b, c, and d

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -4,5 +4,5 @@ python3 peer_node.py \
 	  --name A --listen 0.0.0.0 5002 \
 	  --peers B@192.168.122.3:5003 C@192.168.122.4:5004 D@192.168.122.5:5005 \
 	  --logger 192.168.122.6 5900 \
-	  --offset-ms 600 \
+	  --offset-ms -2000 \
 	  --initiate-broadcast --msg "Hello from A"
--- synchronization/time_sync/node_c/run.bash
+++ synchronization/time_sync/node_c/run.bash
@@ -4,5 +4,5 @@ python3 peer_node.py \
 	  --name C --listen 0.0.0.0 5004 \
 	  --peers A@192.168.122.2:5002 B@192.168.122.3:5003 D@192.168.122.5:5005 \
 	  --logger 192.168.122.6 5900 \
-	  --offset-ms -600 \
+	  --offset-ms 1000 \
       --initiate-broadcast --msg "Hello from C"
