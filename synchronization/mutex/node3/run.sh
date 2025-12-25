0ca31f44cd8d27c22f30efa61b574916ee340439
algof <algofkz@gmail.com>
2025-12-25 21:24:31 +0700

    without mutex

--- synchronization/mutex/node3/run.sh
+++ synchronization/mutex/node3/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./kv.py --id 3 --tcp 8004 --udp 8104 --peers 192.168.122.2:8002=1,192.168.122.3:8003=2,192.168.122.5:8005=4 --logger-addr 192.168.122.6:9000 --numnodes 4 --use-mutex 1 
+python3 ./kv.py --id 3 --tcp 8004 --udp 8104 --peers 192.168.122.2:8002=1,192.168.122.3:8003=2,192.168.122.5:8005=4 --logger-addr 192.168.122.6:9000 --numnodes 4 --use-mutex 0
